deeb8526f19c6edeb2d6cfc8c0ef1f91a3b67227
git에서 Khyeon9 <git mrk19967@gmail.com>
2023-12-03 20:31:24 +0900

    async와 await으로 기본 인증 API호출 작업 및 테스트

--- src/frontend/src/components/todo/security/AuthContext.js
+++ src/frontend/src/components/todo/security/AuthContext.js
@@ -13,6 +13,7 @@ function AuthProvider({ children }) {
     // 3: Provider를 만들어서 State를 context에 넣기
     const [isAuthenticated, setAuthenticated] = useState(false);
     const [username, setUsername] = useState(null);
+    const [token, setToken] = useState(null);
 
     // function login(username, password) {
     //     if(username === 'hyeon' && password === 'dummy') {
@@ -26,32 +27,37 @@ function AuthProvider({ children }) {
     //     }
     // }
 
-    function login(username, password) {
+    async function login(username, password) {
         // base64 incoding
         const basicToken = 'Basic ' + window.btoa(username + ":" + password);
 
-        excuteBasicAuthenticationService(basicToken)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+        try {
+            const response = await excuteBasicAuthenticationService(basicToken);
 
-        setAuthenticated(false);
-        // if(username === 'hyeon' && password === 'dummy') {
-        //     setAuthenticated(true);
-        //     setUsername(username);
-        //     return true;
-        // } else {
-        //     setAuthenticated(false);
-        //     setUsername(null);
-        //     return false;
-        // }
+            if(response.status === 200) {
+                setAuthenticated(true);
+                setUsername(username);
+                setToken(basicToken);
+                return true;
+            } else {
+                logout();
+                return false;
+            }
+        } catch (error) {
+            logout();
+            return false;
+        }
+
     }
 
     function logout() {
         setAuthenticated(false);
+        setUsername(null);
+        setToken(null);
     }
 
     return (
-        <AuthContext.Provider value={ { isAuthenticated, username, login, logout } }>
+        <AuthContext.Provider value={ { isAuthenticated, username, token, login, logout } }>
             { children }
         </AuthContext.Provider>
     );
